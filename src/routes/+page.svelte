0452799a26092c92b8b9abf30dc021ddd164ff3f
Vitor Bacelar <vitor.bacelar@protonmail.com>
2022-08-31 16:11:46 -0300

    config tailwind and call pokemon api

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { pokemon } from '../stores/pokestore';
+
+</script>
+<svelte:head>
+    <title>Svelte Kit Pokedex</title>
+</svelte:head>
+<h1>Svelte kit Pokedex</h1>
+{#each $pokemon as pokeman}
+    <p>{pokeman.name}</p>
+{/each}
