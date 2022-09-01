888da49553257e4f2d1bb4111355e7c120dd69fc
Vitor Bacelar <vitor.bacelar@protonmail.com>
2022-09-01 16:54:09 -0300

    call api

--- src/routes/pokemon/[id]/+page.svelte
+++ src/routes/pokemon/[id]/+page.svelte
@@ -1,5 +1,14 @@
 <script>
     export let data 
     let { pokeman } = data
+    const type = pokeman.types[0].type.name
 </script>
-<h1 class="text-4xl text-center my-8 uppercase">{pokeman.name}</h1>
+<div class="flex flex-col items-center">
+    <h1 class="text-4xl text-center my-8 uppercase">{pokeman.name}</h1>
+    <p>
+        Type: <strong>{type}</strong> | 
+        Height: <strong>{pokeman.height}</strong> | 
+        Weight: <strong>{pokeman.weight}</strong>
+    </p>
+    <img class="card-image" src={pokeman.sprites['front_default']} alt={pokeman.name}>
+</div>
